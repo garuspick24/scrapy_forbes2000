9a0407ee090234168dd656cca307397fa32e05fa
garuspick24 <122586302+garuspick24@users.noreply.github.com>
2024-04-12 16:04:37 +0300

    done

--- forbes/forbes/spiders/company.py
+++ forbes/forbes/spiders/company.py
@@ -1,10 +1,18 @@
 import scrapy
+from scrapy.http import Response
 
 
 class CompanySpider(scrapy.Spider):
     name = "company"
-    allowed_domains = ["forbes,com"]
-    start_urls = ["https://forbes,com"]
+    allowed_domains = ["forbes.com"]
+    start_urls = ["https://www.forbes.com/lists/global2000/?sh=2f25e87d5ac0"]
 
-    def parse(self, response):
-        pass
+    def parse(self, response: Response, **kwargs):
+
+        for group in response.css(".table-row-group"):
+            for company in group.css(".table-row"):
+                yield {
+                    "rank": company.css('.rank::text').get(),
+                    "name": company.css('.organizationName::text').get(),
+                    "link": company.css('.table-row::attr(href)').get()
+                }
